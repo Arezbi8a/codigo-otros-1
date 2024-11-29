08313665434bbb9ed2017be6895e6c9b889995de
Arezbi8a <arezbi8@ciencias.unam.mx>
2024-11-29 12:14:14 -0600

    Ejercicio 1

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,22 +1,27 @@
-const baseEndpoint = 'https://api.github.com';
+const baseEndpoint = "https://api.github.com"; 
 const usersEndpoint = `${baseEndpoint}/users`;
-const $n = document.querySelector('name');
-const $b = document.querySelector('#blog');
+const $n = document.querySelector('.name');
+const $b = document.querySelector('.blog'); //Quitar #
 const $l = document.querySelector('.location');
 
-function displayUser(username) {
+//Debe ser una función asincronica
+
+async function displayUser(username) { 
   $n.textContent = 'cargando...';
-  const response = await fetch(`${usersEndpoint}/${username}`);
-  console.log(data);
-  $n.textContent = '${data.name}';
-  $b.textContent = '${data.blog}';
-  $l.textContent = '${data.location}';
-}
 
-function handleError(err) {
-  console.log('OH NO!');
-  console.log(err);
-  n.textContent = `Algo salió mal: ${err}`
+  //Try va con cath
+  try {
+    const response = await fetch(`${usersEndpoint}/${username}`); 
+  //Necesitamos una const data con json
+    const data = await response.json();
+    console.log(data);
+    $n.textContent = `${data.name}`;
+    $b.textContent = `${data.blog}`;
+    $l.textContent = `${data.location}`;  
+      } catch (err) {      //No necesitamos otra funcion
+    console.log('OH NO!');
+    console.log(err);
+    $n.textContent = `Algo salió mal: ${err}`;
+  }
 }
-
 displayUser('stolinski').catch(handleError);
